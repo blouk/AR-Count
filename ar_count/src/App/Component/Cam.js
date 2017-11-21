b0864c9531e98f786787745cbfe5e29b63955f1d
blouk <nicolas@blouk.com>
2017-11-21 20:14:32 +0100

    [IMP] multi markers

--- ar_count/src/App/Component/Cam.js
+++ ar_count/src/App/Component/Cam.js
@@ -7,15 +7,17 @@ class Cam extends Component {
     constructor(args) {
         super(args);
         this.markers = 0;
-        this.state = {'markers':0};
+        this.state = {
+            'markers': 0
+        };
         this.success = this.success.bind(this);
     }
 
     componentDidMount() {
 
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-        var tw = 1280 / 2;
-        var th = 720 / 2;
+        var tw = 800;
+        var th = 600;
 
         var hdConstraints = {
             audio: false,
@@ -28,7 +30,7 @@ class Cam extends Component {
         };
 
         if (navigator.getUserMedia) {
-            navigator.getUserMedia(hdConstraints, this.success, this.errorCallback,this);
+            navigator.getUserMedia(hdConstraints, this.success, this.errorCallback, this);
         } else {
             this.errorCallback('');
         }
@@ -36,30 +38,75 @@ class Cam extends Component {
 
     success(stream) {
 
-        var self  = this;
         var arController;
-        var  video_element= document.getElementById('video');
+        var video_element = document.getElementById('video');
         video_element.src = window.URL.createObjectURL(stream);
         video_element.play();
         var cameraParam = new window.ARCameraParam();
+        var marker1,marker2,marker3;
 
         cameraParam.onload = function() {
-            var interval = setInterval(function() {
-                if (!video_element.videoWidth)	return;
 
+            var interval = setInterval(function(){
+                if(!video_element.videoWidth) return;
                 if (!arController) {
                     arController = new window.ARController(video_element, cameraParam);
                     arController.setPatternDetectionMode(window.artoolkit.AR_TEMPLATE_MATCHING_MONO_AND_MATRIX);
+
                     arController.debugSetup();
+                    arController.loadMarker('/data/patt.hiro', function(marker) {
+                        //console.log('loaded marker', marker);
+
+                        marker1 = marker;
+
+                    });
+
+                    // arController.loadMarker('/data/pattern-marker2.patt', function(marker) {
+                    //     console.log('loaded marker', marker);
+                    //     marker2 = marker;
+                    //
+                    // });
+                    //
+                    //
+                    // arController.loadMarker('/data/pattern-marker3.patt', function(marker) {
+                    //     console.log('loaded marker', marker);
+                    //     marker3 = marker;
+                    //
+                    // });
+
+                    arController.addEventListener('markerNum', function (markerNum) {
+                        console.log('got markers', markerNum);
+                    });
+
+                    arController.addEventListener('getMarker', function(ev){
+                    //    console.log(ev.data.marker.idPatt);
+                        if(ev.data.marker.idPatt === marker1){
+                            console.log('saw marker', ev.data.marker.idPatt);
+        			        // console.log('transformation matrix', arController.getTransformationMatrix());
+                        }
+                    });
+
                 }
-
                 arController.process();
-                self.setState( {'markers' : arController.getMarkerNum()} );
 
-            }, 16);
+            },16);
+            console.log(arController)
+            // arController.loadMarker('/data/pattern-marker1.patt', function(marker) {
+            //     console.log('loaded marker', marker);
+            //     markerID = marker;
+            //
+            // });
+
+            // arController.addEventListener('getMarker', function(ev){
+            //     if(ev.marker.idPatt === markerID){
+            //         console.log('saw marker', ev.data.marker);
+			//         console.log('transformation matrix', arController.getTransformationMatrix());
+            //     }
+            // });
 
 
         };
+
         cameraParam.src = '/data/camera_para.dat';
     }
 
@@ -70,8 +117,8 @@ class Cam extends Component {
     render() {
         return (
             <div className="ar-cam">
-            <video id="video"/>
-            <div className="ar-count-display"># {this.state.markers}</div>
+                <video id="video" width="640" />
+                <div className = "ar-count-display" >#{this.state.markers}</div>
             </div>
         );
     }
